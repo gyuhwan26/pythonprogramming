2fbd067e7c63ad83cdf19686684c742a30939977
gyuhwan26 <name2062@naver.com>
2026-03-13 12:28:04 +0900

    Add files via upload

---
+++ CircleCal.py
@@ -0,0 +1,2 @@
+radius = float(input("원의 반지름을 입력하세요> "))
+print("원의 둘레 :",6.28*radius," 원의 넓이 :",3.14*radius**2)
---
+++ kgToPound.py
@@ -0,0 +1,2 @@
+weight = float(input("무게(kg)를 입력하세요> "))
+print(weight,'kg는 ',weight*2.2046226218,'lb 입니다.')
