dbaf71b99a9165447843bf91d341b7692c07a317
gyuhwan26 <name2062@naver.com>
2026-04-15 21:13:00 +0900

    6주차 1.py

--- 6주차 1.py
+++ 6주차 1.py
@@ -1,5 +1,22 @@
 import random
-print("** 로또 추첨을 시작합니다. **")
-print("\n추첨된 로또 번호 ==>",end=" ")
-for a in range(1,7):
-    print(random.randrange(1,46),end=" ")
+def getNumber():
+    return random.randrange(1,46)
+
+lotto = []
+num = 0
+
+print("** 로또 추첨을 시작합니다. ** \n")
+
+while True:
+    num = getNumber()
+
+    if lotto.count(num) == 0:
+        lotto.append(num)
+    
+    if len(lotto) >= 6:
+        break
+
+print("추첨된 로또 번호 ==> ",end='')
+lotto.sort()
+for i in range(0,6):
+    print("%d "%lotto[i], end=' ')
